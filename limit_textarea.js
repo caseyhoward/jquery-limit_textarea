4d97397f0bd139c05b938a1c325024fe3c4c8206
Casey Howard <choward@M080701.local>
2010-01-28 12:27:14 -0800

    Fixed some bugs. Everything as of now appears to be working.

--- limit_textarea.js
+++ limit_textarea.js
@@ -4,8 +4,7 @@
     $.fn.limit_textarea = function(options){
         return this.each(function() { 
             options = $.extend({}, $.textarea_limiter.defaults, options);
-            var textarea_limiter = new $.textarea_limiter($(this), options)
-            
+            var textarea_limiter = new $.textarea_limiter($(this), options);
             if (options['show_immediately']) {
                 textarea_limiter.update_description();
             }
@@ -20,27 +19,30 @@
     }
     
     $.textarea_limiter = function(textarea_jquery, options) {
-        this.textarea_jquery = textarea_jquery;
         this.options = options;
+        this.textarea_jquery = textarea_jquery;
+        this.description_id = this.textarea_jquery[0].id + "_limit_description";
+        
+        var existing_descriptions = jQuery('#' + this.description_id);
+        if (existing_descriptions.length == 0) {
+            var description_div = '<div id="' + this.description_id + '"></div>';
+            if (options['placement'] == 'after') {
+                this.textarea_jquery.after(description_div);
+            } else if (options['placement'] == 'before') {
+                this.textarea_jquery.before(description_div);
+            } else {
+                alert("limit_textarea: Invalid placement");
+            }
+        }
+        
+        this.description_jquery = $('#' + this.description_id);
         
         this.update_description = function() {
-            var description_id = this.textarea_jquery[0].id + "_limit_description";
-            var existing_descriptions = jQuery('#' + description_id);
             var description_string;
-            if (existing_descriptions.length == 0) {
-                if (options['placement'] == 'after') {
-                    this.textarea_jquery.after('<div id="' + description_id + '"></div>');
-                } else if (options['placement'] == 'before') {
-                    this.textarea_jquery.before('<div id="' + description_id + '"></div>');
-                } else {
-                    alert("limit_textarea: Invalid placement");
-                }
-            }
             var count = this.get_count();
             if (count > options['max']) {
                 description_class = options['error_class'];
                 description_string = (count - options['max']) + ' ' + options['type'] + 's over the limit (max: ' + options['max'] + ')';
-                return_value = false;
             } else {
                 if ((options['max'] - count) < options['warning_max']) {
                     description_class = options['warning_class'];
@@ -49,31 +51,34 @@
                 }
                 description_string = (options['max'] - count) + ' ' + options['type'] + 's remaining.';
             }
-            description_jquery = jQuery('#' + description_id);
-            jQuery.each([options['error_class'], options['warning_class'], options['success_class']], function(index, class_name) {
+            var description_jquery = this.description_jquery; // So we can access it in $.each function, kinda lame
+            $.each([this.options['error_class'], this.options['warning_class'], this.options['success_class']], function(index, class_name) {
                 description_jquery.removeClass(class_name);
             });
-            description_jquery.addClass(description_class);
-            description_jquery.html(description_string);
+            this.description_jquery.addClass(description_class);
+            this.description_jquery.html(description_string);
         };
+        
         this.check_character = function(e) {
             var count = this.get_count();
-            if (e.charCode == 0) {
+            // If the key isn't refer to an actual character, then it's good no matter what
+            if (e.charCode == 0 || options['strict'] == false) {
                 return true;
             }
             if (count > this.options['max'] && e.which != 8 && e.which != 46) {
                 return false;
             } else {
                 // If we're limiting by words, using strict, and at the max and the user tries to enter a space then don't let them
-                if (this.options['type'] == 'word' && count == this.options['max'] && this.options['strict'] && e.charcode != 0) {
+                if (this.options['type'] == 'word' && count == this.options['max'] && e.charcode != 0) {
                     return false;
-                } else if (this.options['type'] == 'character' && count == this.options['max'] && this.options['strict']) {
+                } else if (this.options['type'] == 'character' && count == this.options['max']) {
                     return false;
                 }
 
             }
             return true;
         };
+        
         this.get_count = function () {
             if (this.options['type'] == 'character') {
                 return this.textarea_jquery.val().length;
